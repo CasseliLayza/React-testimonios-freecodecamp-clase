99ccf1813634c3afe304b8de4d88f0ada52aa51a
CasseliLayza <casseli.layza@gmail.com>
2024-07-21 21:33:24 -0500

    Init Testimonios Ract Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
+import React from "react";
 import "./App.css";
 import { Testimonio } from "./componentes/Testimonio";
 import testimonios from "./datosTestimonios/testimonios";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="contenedor-principal">
-        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        {testimonios.map((testimonio, index) => (
-          <Testimonio
-            key={index}
-            nombre={testimonio.nombre}
-            pais={testimonio.pais}
-            cargo={testimonio.cargo}
-            empresa={testimonio.empresa}
-            testimonio={testimonio.testimonio}
-            nombreImagen={testimonio.nombreImagen}
-          />
-        ))}
+
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <div className="contenedor-principal">
+          <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+          {testimonios.map((testimonio, index) => (
+            <Testimonio
+              key={index}
+              nombre={testimonio.nombre}
+              pais={testimonio.pais}
+              cargo={testimonio.cargo}
+              empresa={testimonio.empresa}
+              testimonio={testimonio.testimonio}
+              nombreImagen={testimonio.nombreImagen}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 
 export default App;
--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import  "../hojasDeEstilos/Testimonio.css";
 
-export function Testimonio(props){
-    return(
-        <div className="contenedor-testimonio">
-            <img
-            className="imagen-testimonio"
-            src={require(`../imagenes/${props.nombreImagen}.png`)}
-            alt='Foto de' />
-
-            <div className="contenedor-texto-testimonio">
-                <p className="nombre-testimonio" ><strong>{props.nombre}</strong> en <strong>{props.pais}</strong></p>
-                <p className="cargo-testimonio" >{props.cargo} en <strong>{props.empresa}</strong></p>
-                <p className="texto-testimonio" >"{props.testimonio}"</p>
+export class Testimonio extends React.Component{
+     render(){
+        return(
+            <div className="contenedor-testimonio">
+                <img
+                className="imagen-testimonio"
+                src={require(`../imagenes/${this.props.nombreImagen}.png`)}
+                alt={`Foto de ${this.props.nombreImagen}`} />
+    
+                <div className="contenedor-texto-testimonio">
+                    <p className="nombre-testimonio" ><strong>{this.props.nombre}</strong> en <strong>{this.props.pais}</strong></p>
+                    <p className="cargo-testimonio" >{this.props.cargo} en <strong>{this.props.empresa}</strong></p>
+                    <p className="texto-testimonio" >"{this.props.testimonio}"</p>
+                </div>
+    
+     
             </div>
-
- 
-        </div>
-    );
-   
+        );
+    }
 }
-
 
 
 // export default Testimonio;
